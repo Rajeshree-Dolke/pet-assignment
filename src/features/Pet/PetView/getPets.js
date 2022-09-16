e7b1dbf8f189f3c56b6826042e116146c3c543f3
rdolke <r.dolke@f5.com>
2022-09-16 12:05:25 +0530

    fix: security issue

--- src/features/Pet/PetView/getPets.js
+++ src/features/Pet/PetView/getPets.js
@@ -1,6 +1,5 @@
 export async function getPets() {
     const url = process.env.NODE_ENV === 'development' ? process.env.REACT_APP_LOCAL_BACKEND : process.env.REACT_APP_PROD_BACKEND
-    console.log(url,'h')
     try {
         const response = await fetch(url + '/pets')
         if (response.status === 200) {
